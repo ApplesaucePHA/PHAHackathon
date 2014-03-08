53acd34247e18803050eb2540141a87ccfa2f7ed
Kirk Musngi <musngikd@gmail.com>
2014-03-08 18:20:25 -0500

    Initial project commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PHAHackathon::Application.config.secret_key_base = 'f26b097a11a419bdd70b684d1c82bc1eeb000bdf08e77446db3bc0c4e4d33794e4f8b2260745b26153295d049a265582143be8f32180b6f1f3e58f1762f4fd51'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PHAHackathon::Application.config.session_store :cookie_store, key: '_PHAHackathon_session'
