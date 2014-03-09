267203655f70eb5e476f2c4b148f6f1dd138a931
Kirk Musngi <musngikd@gmail.com>
2014-03-08 19:52:39 -0500

    Begin modeling and creating teacher models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 PHAHackathon::Application.routes.draw do
+  resources :teachers
+
+  root 'static_pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
